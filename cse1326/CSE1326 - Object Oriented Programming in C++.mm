3b459c3a1c377d7274b339e673a4bd62ddeb136c
Bud Davis <jimmie.davis@uta.edu>
2025-06-28 22:23:52 -0500

    work in progress.

--- cse1326/CSE1326 - Object Oriented Programming in C++.mm
+++ cse1326/CSE1326 - Object Oriented Programming in C++.mm
@@ -1,17 +1,17 @@
 <map version="freeplane 1.12.1">
 <!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
-<node TEXT="Object Oriented Programming in C++" FOLDED="false" ID="ID_191153586" CREATED="1610381622013" MODIFIED="1747960514807" ICON_SIZE="36 pt" LINK="menuitem:_ExternalImageAddAction" STYLE="oval">
-<font SIZE="22"/>
+<node TEXT="Object Oriented Programming in C++" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1751167266939" STYLE="oval" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_FLOW">
+<font SIZE="18"/>
 <hook NAME="MapStyle">
-    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_icon_for_attributes="true" show_notes_in_map="false" show_tags="UNDER_NODES" associatedTemplateLocation="template:/BuzanMap.mm" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="false" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_tags="UNDER_NODES" showTagCategories="false" show_note_icons="true" show_icon_for_attributes="true"/>
     <tags category_separator="::"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="default" ID="ID_12905907" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
-<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_12905907" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
 <font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
 <richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
 <richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
@@ -40,160 +40,48 @@
 <stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
 <font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="styles.important" ID="ID_520072399">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
 <icon BUILTIN="yes"/>
-<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_520072399"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
 </stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64 pt" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
-<font NAME="Segoe Print" SIZE="22"/>
-<edge COLOR="#ffffff"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
-<font SIZE="18" BOLD="false" ITALIC="true"/>
-<edge STYLE="sharp_bezier" WIDTH="8"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
 <font SIZE="16"/>
-<edge STYLE="bezier" WIDTH="4"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
 <font SIZE="14"/>
-<edge STYLE="bezier" WIDTH="3"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
-<font SIZE="13"/>
-<edge STYLE="bezier" WIDTH="2"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24 px" BORDER_WIDTH_LIKE_EDGE="true">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
 <font SIZE="12"/>
-<edge STYLE="bezier" WIDTH="1"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24 px">
-<edge STYLE="bezier"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16 px">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
 <font SIZE="10"/>
-<edge STYLE="bezier"/>
 </stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16 px">
-<edge STYLE="bezier"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16 px">
-<edge STYLE="bezier"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16 px">
-<edge STYLE="bezier"/>
-</stylenode>
-<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16 px">
-<edge STYLE="bezier"/>
-</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
 </stylenode>
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="15" RULE="ON_BRANCH_CREATION"/>
-<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<edge COLOR="#ffffff"/>
-<richcontent TYPE="DETAILS">
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      1. Write the central idea
-    </p>
-    <p>
-      2. Save the map
-    </p>
-    <p>
-      3. Select central node,
-    </p>
-    <p>
-      &#xa0;&#xa0;&#xa0;&#xa0;click on icon to add picture
-    </p>
-    <p>
-      4. Delete menu links Add image..
-    </p>
-    <p>
-      &#xa0;&#xa0;&#xa0;&#xa0;with CTRL+K and delete field with link
-    </p>
-    <p>
-      5. Delete node details with right click on mouse
-    </p>
-    <p>
-      &#xa0;&#xa0;&#xa0;&#xa0;on central node - Remove node details
-    </p>
-  </body>
-</html></richcontent>
-<node TEXT="" POSITION="bottom_or_right" ID="ID_273436263" CREATED="1610381622018" MODIFIED="1747960511213" HGAP_QUANTITY="91.2683 pt" VSHIFT_QUANTITY="-30.73171 pt">
-<edge COLOR="#007c7c"/>
-<richcontent TYPE="NOTE">
-<html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      xxxxx
-    </p>
-  </body>
-</html></richcontent>
-<node TEXT="IDEA 1.1" ID="ID_588194298" CREATED="1610381622019" MODIFIED="1610381622019" HGAP_QUANTITY="15.7561 pt" VSHIFT_QUANTITY="-12.29268 pt">
-<node TEXT="" ID="ID_1824120200" CREATED="1749953579991" MODIFIED="1749953604160">
-<node TEXT="" ID="ID_542118861" CREATED="1749953581506" MODIFIED="1749953581506"/>
-</node>
-</node>
-<node TEXT="IDEA 1.2" ID="ID_1104103844" CREATED="1610381622019" MODIFIED="1610381622019">
-<node TEXT="" ID="ID_1765905154" CREATED="1749953579104" MODIFIED="1749953579104"/>
-</node>
-</node>
-<node TEXT="IDEA 2" POSITION="bottom_or_right" ID="ID_495676614" CREATED="1610381622019" MODIFIED="1610381622019" HGAP_QUANTITY="101.80488 pt" VSHIFT_QUANTITY="-42.14634 pt">
-<edge COLOR="#009900"/>
-<node TEXT="IDEA 2.1" ID="ID_744391648" CREATED="1610381622019" MODIFIED="1610381622019" HGAP_QUANTITY="15.7561 pt" VSHIFT_QUANTITY="-14.92683 pt">
-<node TEXT="" ID="ID_1519362580" CREATED="1749953577073" MODIFIED="1749953577073"/>
-</node>
-<node TEXT="IDEA 2.2" ID="ID_903669612" CREATED="1610381622019" MODIFIED="1610381622019">
-<node TEXT="" ID="ID_1347359061" CREATED="1749953578021" MODIFIED="1749953578021"/>
-</node>
-</node>
-<node TEXT="IDEA 3" POSITION="bottom_or_right" ID="ID_90823870" CREATED="1610381622019" MODIFIED="1610381622019" HGAP_QUANTITY="56.14634 pt" VSHIFT_QUANTITY="-61.46342 pt">
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="original syllabus" POSITION="bottom_or_right" ID="ID_241450616" CREATED="1751167227410" MODIFIED="1751167234246">
 <edge COLOR="#ff0000"/>
-<node TEXT="IDEA 3.1" ID="ID_1201198400" CREATED="1610381622019" MODIFIED="1610381622019" HGAP_QUANTITY="15.7561 pt" VSHIFT_QUANTITY="-15.80488 pt">
-<node TEXT="" ID="ID_722559318" CREATED="1749953574659" MODIFIED="1749953574659"/>
 </node>
-<node TEXT="IDEA 3.2" ID="ID_1615683347" CREATED="1610381622019" MODIFIED="1610381622019" HGAP_QUANTITY="17.5122 pt" VSHIFT_QUANTITY="3.5122 pt">
-<node TEXT="" ID="ID_1651080153" CREATED="1749953575888" MODIFIED="1749953575888"/>
-</node>
-</node>
-<node TEXT="IDEA 6" POSITION="top_or_left" ID="ID_25434398" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="73.70732 pt" VSHIFT_QUANTITY="-37.7561 pt">
-<edge COLOR="#cc00ff"/>
-<node TEXT="IDEA 6.1" ID="ID_352937383" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="13.12195 pt" VSHIFT_QUANTITY="-9.65854 pt">
-<node TEXT="" ID="ID_1154470799" CREATED="1749953565125" MODIFIED="1749953565125"/>
-</node>
-<node TEXT="IDEA 6.2" ID="ID_966834988" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="15.7561 pt" VSHIFT_QUANTITY="-7.90244 pt">
-<node TEXT="" ID="ID_604796833" CREATED="1749953566768" MODIFIED="1749953566768"/>
-</node>
-</node>
-<node TEXT="IDEA 5" POSITION="top_or_left" ID="ID_1822402846" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="84.24391 pt" VSHIFT_QUANTITY="-36 pt">
-<edge COLOR="#ff9900"/>
-<node TEXT="IDEA 5.1" ID="ID_1274507163" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="14 pt" VSHIFT_QUANTITY="-21.07317 pt">
-<node TEXT="" ID="ID_1119294011" CREATED="1749953568044" MODIFIED="1749953568044"/>
-</node>
-<node TEXT="IDEA 5.2" ID="ID_1203076641" CREATED="1610381622020" MODIFIED="1610381622020">
-<node TEXT="" ID="ID_368300427" CREATED="1749953569351" MODIFIED="1749953569351"/>
-</node>
-</node>
-<node TEXT="IDEA 4" POSITION="top_or_left" ID="ID_575079876" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="67.56098 pt" VSHIFT_QUANTITY="-42.14634 pt">
+<node TEXT="01 The Base Language" POSITION="top_or_left" ID="ID_1961025307" CREATED="1751167247591" MODIFIED="1751167275772">
 <edge COLOR="#0000ff"/>
-<node TEXT="IDEA 4.1" ID="ID_317662625" CREATED="1610381622020" MODIFIED="1610381622020" HGAP_QUANTITY="14.87805 pt" VSHIFT_QUANTITY="-18.43903 pt">
-<node TEXT="" ID="ID_941776696" CREATED="1749953571501" MODIFIED="1749953571501"/>
 </node>
-<node TEXT="IDEA 4.2" ID="ID_1522387997" CREATED="1610381622021" MODIFIED="1610381622021">
-<node TEXT="" ID="ID_291366420" CREATED="1749953572750" MODIFIED="1749953572750"/>
-</node>
+<node TEXT="02 Windows and Graphics" POSITION="top_or_left" ID="ID_134676722" CREATED="1751167278204" MODIFIED="1751167300726">
+<edge COLOR="#00ff00"/>
 </node>
 </node>
 </map>
