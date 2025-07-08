700d0cf9d89974ebe989245883e9b7d10a6f9f01
Bud Davis <jimmie.davis@uta.edu>
2025-07-08 16:15:09 -0500

    Chapter 3 is completed.

--- cse1326/CSE1326 - Object Oriented Programming in C++.mm
+++ cse1326/CSE1326 - Object Oriented Programming in C++.mm
@@ -73,7 +73,7 @@
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="25" RULE="ON_BRANCH_CREATION"/>
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
-<node TEXT="01 Introduction" POSITION="bottom_or_right" ID="ID_873130339" CREATED="1751483600281" MODIFIED="1751644672410">
+<node TEXT="01 Introduction" FOLDED="true" POSITION="bottom_or_right" ID="ID_873130339" CREATED="1751483600281" MODIFIED="1751644672410">
 <edge COLOR="#ff00ff"/>
 <node TEXT="01 Objectives" FOLDED="true" ID="ID_1506017022" CREATED="1751483877215" MODIFIED="1751484277618">
 <node TEXT="01 Understand the compilation process and the type of errors emitted from each stage." ID="ID_725997858" CREATED="1751484514630" MODIFIED="1751484549807"/>
@@ -84,7 +84,7 @@
 <node TEXT="06 Students will be able to create a private github repository and add collaborators" ID="ID_33175148" CREATED="1751484355600" MODIFIED="1751484574959"/>
 <node TEXT="07 Students will be able to use github for simple source control activities.  (add, commit, push, pull, clone." ID="ID_447166497" CREATED="1751484396999" MODIFIED="1751484579303"/>
 </node>
-<node TEXT="02 Lecture" FOLDED="true" ID="ID_537497064" CREATED="1751484315096" MODIFIED="1751644675829">
+<node TEXT="02 Lecture" ID="ID_537497064" CREATED="1751484315096" MODIFIED="1751644675829">
 <node TEXT="01 Valid  Program" ID="ID_899763014" CREATED="1751484613356" MODIFIED="1751484790971">
 <node TEXT="01 main" ID="ID_303604521" CREATED="1751484709107" MODIFIED="1751484742137"/>
 <node TEXT="02 headers" ID="ID_1931172698" CREATED="1751484733050" MODIFIED="1751484746235"/>
@@ -144,7 +144,7 @@
 </node>
 </node>
 </node>
-<node TEXT="03 Types" POSITION="top_or_left" ID="ID_1121057718" CREATED="1751483632723" MODIFIED="1751483640874">
+<node TEXT="03 Types" FOLDED="true" POSITION="top_or_left" ID="ID_1121057718" CREATED="1751483632723" MODIFIED="1751483640874">
 <edge COLOR="#7c0000"/>
 <node TEXT="01 Objectives" FOLDED="true" ID="ID_1080385548" CREATED="1751484245090" MODIFIED="1751484297218">
 <node TEXT="01 Student will be able to list all of the fundamental types and describe the differences between them." ID="ID_185538877" CREATED="1751824788699" MODIFIED="1751828683464"/>
@@ -159,7 +159,7 @@
 <node TEXT="10 Students will be able to define and use unions, to include understanding the underlying mechanism" ID="ID_1401829537" CREATED="1751828996403" MODIFIED="1751829600349"/>
 <node TEXT="11 Students will be able to define and initialize classes, to include specifying appropriate visibility modifiers." ID="ID_1146871131" CREATED="1751829042235" MODIFIED="1751829092745"/>
 </node>
-<node TEXT="02 Lecture" ID="ID_1225056246" CREATED="1751484250994" MODIFIED="1751484301482">
+<node TEXT="02 Lecture" ID="ID_1225056246" CREATED="1751484250994" MODIFIED="1752008595517">
 <node TEXT="Fundamental Types" ID="ID_405844597" CREATED="1751830490228" MODIFIED="1751831625348"><richcontent TYPE="NOTE">
 <html>
   <head>
@@ -211,8 +211,13 @@
 </node>
 <node TEXT="deault is double, can add f (F)for single precision or l (L) for extended precision" ID="ID_1939368218" CREATED="1751832822988" MODIFIED="1751832906534"/>
 </node>
-<node TEXT="Char" ID="ID_1034917615" CREATED="1751830928661" MODIFIED="1751830946728"/>
-<node TEXT="Boolean" FOLDED="true" ID="ID_642346090" CREATED="1751830948356" MODIFIED="1751831965462"><richcontent TYPE="NOTE">
+<node TEXT="Char" ID="ID_1034917615" CREATED="1751830928661" MODIFIED="1751830946728">
+<node TEXT="&quot;wide&quot; characters out of scope for this class !!! But, they do exist." ID="ID_1031904391" CREATED="1752007566277" MODIFIED="1752007598125"/>
+<node TEXT="1 byte (8 bits) .... for the last 50 years or so....." ID="ID_368877291" CREATED="1752007600398" MODIFIED="1752007652656"/>
+<node TEXT="can be signed or unsigned" ID="ID_1387444265" CREATED="1752007677149" MODIFIED="1752007684568"/>
+<node TEXT="&apos;x&apos; single quote literals" ID="ID_1203276930" CREATED="1752007695564" MODIFIED="1752007706568"/>
+</node>
+<node TEXT="Boolean" ID="ID_642346090" CREATED="1751830948356" MODIFIED="1751831965462"><richcontent TYPE="NOTE">
 <html>
   <head>
     
@@ -263,28 +268,205 @@
 <node TEXT="1 or 0" ID="ID_1502974021" CREATED="1751831685092" MODIFIED="1751831691969"/>
 </node>
 </node>
-<node TEXT="operators" ID="ID_126544184" CREATED="1751830519546" MODIFIED="1751831027822"/>
-<node TEXT="std::byte" ID="ID_548477926" CREATED="1751830533113" MODIFIED="1751830537957"/>
-<node TEXT="size_t, sizeof()" ID="ID_1321445553" CREATED="1751830543033" MODIFIED="1751830560494"/>
-<node TEXT="void" ID="ID_1455678421" CREATED="1751830575104" MODIFIED="1751830577172"/>
-<node TEXT="arrays" ID="ID_558536979" CREATED="1751830579271" MODIFIED="1751830583539"/>
-<node TEXT="for loops" ID="ID_173014708" CREATED="1751830590631" MODIFIED="1751830593195"/>
-<node TEXT="C strings" ID="ID_1376421849" CREATED="1751830606447" MODIFIED="1751830611298"/>
+<node TEXT="operators" ID="ID_126544184" CREATED="1751830519546" MODIFIED="1751831027822">
+<node TEXT="comparison" ID="ID_394188424" CREATED="1752007767178" MODIFIED="1752007821467"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      ==,!=,&lt;,&gt;,&gt;=.&lt;=
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="logical" ID="ID_1484289335" CREATED="1752007773877" MODIFIED="1752007777497">
+<node TEXT="!" ID="ID_838756964" CREATED="1752007856832" MODIFIED="1752007863868"/>
+</node>
+<node TEXT="binary" ID="ID_594036606" CREATED="1752007832424" MODIFIED="1752007835992">
+<node TEXT="&amp;&amp;,AND" ID="ID_417073088" CREATED="1752007870247" MODIFIED="1752007880543"/>
+<node TEXT="||,OR" ID="ID_476993341" CREATED="1752007881951" MODIFIED="1752007886231"/>
+</node>
+<node TEXT="ternary" ID="ID_498814073" CREATED="1752007894703" MODIFIED="1752007944977"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <span style="color: rgb(0, 29, 53); font-family: monospace; font-size: 14px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: pre; background-color: rgba(245, 248, 255, 0.5); display: inline !important; float: none;">condition ? expression_if_true : expression_if_false;</span>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="bitwise" ID="ID_1468831041" CREATED="1752008127350" MODIFIED="1752008202782"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &amp;,|,~/&lt;&lt;.&gt;&gt;,!
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node TEXT="std::byte" ID="ID_548477926" CREATED="1751830533113" MODIFIED="1751830537957">
+<node TEXT="#include &lt;cstddef&gt;" ID="ID_1708664394" CREATED="1752008051486" MODIFIED="1752008067640"/>
+<node TEXT="only supports bitwise operators" ID="ID_1511654122" CREATED="1752008105806" MODIFIED="1752008120589"/>
+</node>
+<node TEXT="size_t, sizeof()" ID="ID_1321445553" CREATED="1751830543033" MODIFIED="1751830560494">
+<node TEXT="It operates on a single operand, which can be a type name (enclosed in parentheses, e.g., sizeof(int)) or an expression (where parentheses are optional, e.g., sizeof x for a variable x). This syntax is characteristic of an operator." ID="ID_938268005" CREATED="1752007988534" MODIFIED="1752007994037"/>
+<node TEXT="size_t will store the largest pointer on the machine, so it can be used to access any symbol." ID="ID_89640118" CREATED="1752007998333" MODIFIED="1752008019328"/>
+<node TEXT="size_t is also in namespace std, std::size_t" ID="ID_102369874" CREATED="1752008079862" MODIFIED="1752008096558"/>
+<node TEXT="#include &lt;iterable&gt; gives a size() method" ID="ID_1899973744" CREATED="1752008531233" MODIFIED="1752008554719"/>
+<node TEXT="size_of(array)/size_of( int)" ID="ID_1432919437" CREATED="1752008555366" MODIFIED="1752008575587"/>
+</node>
+<node TEXT="void" ID="ID_1455678421" CREATED="1751830575104" MODIFIED="1751830577172">
+<node TEXT="" ID="ID_1626965572" CREATED="1752008257695" MODIFIED="1752008257695">
+<node ID="ID_1295579187" CREATED="1752008268289" MODIFIED="1752008268289"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <code class="mv6bHd" style="background-position: 0% 0%; background-repeat: repeat; background-attachment: scroll; background-color: rgb(229, 237, 255); background-image: null; font-family: monospace; padding-top: 0px; padding-bottom: 0px; padding-right: 4px; padding-left: 4px; color: rgb(0, 29, 53); font-size: 18px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: normal"><span style="background-position: 0% 0%; background-repeat: repeat; background-attachment: scroll; background-color: rgb(229, 237, 255); background-image: null; font-family: monospace; padding-top: 0px; padding-bottom: 0px; padding-right: 4px; padding-left: 4px; color: rgb(0, 29, 53); font-size: 18px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: normal;">void</span></code><span style="color: rgb(0, 29, 53); font-family: Google Sans, Roboto, Arial, sans-serif; font-size: 18px; font-style: normal; font-weight: 400; letter-spacing: normal; text-align: start; text-indent: 0px; text-transform: none; word-spacing: 0px; white-space: normal; background-color: rgb(255, 255, 255); display: inline !important; float: none;">&#xa0;keyword signifies the absence of a type or value</span>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node TEXT="void* means pointer of unknown type" ID="ID_784953889" CREATED="1752008270813" MODIFIED="1752008284901"/>
+</node>
+<node TEXT="arrays" ID="ID_558536979" CREATED="1751830579271" MODIFIED="1751830583539">
+<node TEXT="contiguous collection of a type" ID="ID_1643305569" CREATED="1752008287098" MODIFIED="1752008324369"/>
+<node TEXT="(contained type) name [size]" ID="ID_477457519" CREATED="1752008330116" MODIFIED="1752008354317"/>
+<node TEXT="Initialization with braces" ID="ID_1603562210" CREATED="1752008356608" MODIFIED="1752008366709"/>
+<node TEXT="size can be inferred from number of items in initializer" ID="ID_535194138" CREATED="1752008370838" MODIFIED="1752008389955"/>
+</node>
+<node TEXT="for loops" ID="ID_173014708" CREATED="1751830590631" MODIFIED="1751830593195">
+<node TEXT="old school" ID="ID_800346163" CREATED="1752008452663" MODIFIED="1752008466987">
+<node TEXT="for (int i=0;i&lt;10;i++)" ID="ID_310400919" CREATED="1752008474578" MODIFIED="1752008490600"/>
+</node>
+<node TEXT="iterator" ID="ID_1433506013" CREATED="1752008468926" MODIFIED="1752008473749">
+<node TEXT="for ( int X: xlist)" ID="ID_404590527" CREATED="1752008491204" MODIFIED="1752008530325"/>
+</node>
+</node>
+<node TEXT="C strings" ID="ID_1376421849" CREATED="1751830606447" MODIFIED="1752008595512"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      as in 'arrays of character' not pointer to an array of characters,
+    </p>
+  </body>
+</html></richcontent>
+<node TEXT="char a[]=&quot;abc&quot;;" ID="ID_1294626417" CREATED="1752008611108" MODIFIED="1752008635787">
+<node TEXT="" ID="ID_1063483858" CREATED="1752008639539" MODIFIED="1752008639539"/>
+</node>
+<node TEXT="if using char*, need to cas to &apos;char*&apos;" ID="ID_196087181" CREATED="1752008643002" MODIFIED="1752008667617"/>
+<node TEXT="man ascii" ID="ID_1031605430" CREATED="1752008668897" MODIFIED="1752008672738"/>
+</node>
 <node TEXT="User Defined Types" ID="ID_287809503" CREATED="1751830615942" MODIFIED="1751830621322">
 <node TEXT="Enumerations" ID="ID_443003384" CREATED="1751830627574" MODIFIED="1751830633710">
-<node TEXT="scoped enumerations" ID="ID_1190394976" CREATED="1751830634750" MODIFIED="1751830658849"/>
+<node TEXT="scoped enumerations" ID="ID_1190394976" CREATED="1751830634750" MODIFIED="1752008831676"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      enum class color {RED,GREEN,BLUE};
+    </p>
+    <p>
+      color x= color::GREEN;
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      switch ( a )
+    </p>
+    <p>
+      {
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;&#xa0;color::RED:
+    </p>
+    <p>
+      &#xa0;&#xa0;&#xa0;color::BLUE:
+    </p>
+    <p>
+      }
+    </p>
+  </body>
+</html></richcontent>
+</node>
 <node TEXT="old school enumerations" ID="ID_786541423" CREATED="1751830643139" MODIFIED="1751836068380"/>
 </node>
-<node TEXT="Classes (POD)" ID="ID_1170098796" CREATED="1751830677628" MODIFIED="1751830684076"/>
-<node TEXT="Unions" ID="ID_269435291" CREATED="1751830686956" MODIFIED="1751830692240"/>
-<node TEXT="Classes" ID="ID_1523840678" CREATED="1751830700259" MODIFIED="1751830704478"/>
+<node TEXT="Classes (POD)" ID="ID_1170098796" CREATED="1751830677628" MODIFIED="1751830684076">
+<node TEXT="Pod == Plain Old Data" ID="ID_888859777" CREATED="1752008834978" MODIFIED="1752008848187"/>
+<node TEXT="Can copy it around as bytes" ID="ID_1936858867" CREATED="1752008854394" MODIFIED="1752008870326"/>
+</node>
+<node TEXT="Unions" ID="ID_269435291" CREATED="1751830686956" MODIFIED="1752008953772"><richcontent TYPE="NOTE">
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      union variant {
+    </p>
+    <p>
+      &#xa0;&#xa0;char a;
+    </p>
+    <p>
+      &#xa0;&#xa0;unsigned char b;
+    </p>
+    <p>
+      &#xa0;&#xa0;int c;
+    </p>
+    <p>
+      ;}
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      u t;
+    </p>
+    <p>
+      t.a = 'a';
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node TEXT="Classes" ID="ID_1523840678" CREATED="1751830700259" MODIFIED="1751830704478">
+<node TEXT="Use class or struct" ID="ID_1429482847" CREATED="1752008967990" MODIFIED="1752008979666">
+<node TEXT="depends on default visibility, use class and they are private by default" ID="ID_194187976" CREATED="1752008981214" MODIFIED="1752009075831"/>
+</node>
+<node TEXT="Access specifiers" ID="ID_439489375" CREATED="1752009141826" MODIFIED="1752009159599">
+<node TEXT="public" ID="ID_464484778" CREATED="1752008996061" MODIFIED="1752009085711"/>
+<node TEXT="private" ID="ID_1388679109" CREATED="1752009088323" MODIFIED="1752009091816">
+<node TEXT="only in the class in which it is defined" ID="ID_877119396" CREATED="1752009101931" MODIFIED="1752009111503"/>
+</node>
+<node TEXT="protected" ID="ID_800623865" CREATED="1752009093347" MODIFIED="1752009098801">
+<node TEXT="defined class, derived classes and friends" ID="ID_1879840729" CREATED="1752009207937" MODIFIED="1752009239639"/>
 </node>
 </node>
 </node>
-<node TEXT="04 Reference Types" POSITION="top_or_left" ID="ID_92519863" CREATED="1751483644211" MODIFIED="1751485437334">
+</node>
+</node>
+</node>
+<node TEXT="04 Reference Types" FOLDED="true" POSITION="top_or_left" ID="ID_92519863" CREATED="1751483644211" MODIFIED="1751993744758">
 <edge COLOR="#00007c"/>
 <node TEXT="01 Objectives" ID="ID_495924139" CREATED="1751485076654" MODIFIED="1751485082321"/>
-<node TEXT="02 Lecture" ID="ID_1624682033" CREATED="1751485084813" MODIFIED="1751485437333"/>
+<node TEXT="02 Lecture" ID="ID_1624682033" CREATED="1751485084813" MODIFIED="1751993744756"/>
 </node>
 <node TEXT="05 Object Lifecycle" FOLDED="true" POSITION="top_or_left" ID="ID_13489677" CREATED="1751483660899" MODIFIED="1751483669320">
 <edge COLOR="#007c00"/>
