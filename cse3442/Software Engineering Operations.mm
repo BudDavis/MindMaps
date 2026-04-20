be04c9b90d28214b1a930af008c3aa349553bf5f
Bud Davis <jimmie.davis@uta.edu>
2026-04-20 08:43:24 -0500

    more.

---
+++ cse3442/Software Engineering Operations.mm
@@ -0,0 +1,125 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="Software Engineering Operations" FOLDED="false" ID="ID_696401721" CREATED="1610381621824" MODIFIED="1776448943380" STYLE="oval" CHILD_NODES_LAYOUT="TOPTOBOTTOM_RIGHT_CENTERED">
+<font SIZE="18"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" fit_to_viewport="false" show_icons="BESIDE_NODES" associatedTemplateLocation="template:/standard-1.6.mm" show_tags="UNDER_NODES" showTagCategories="false" show_note_icons="true" show_icon_for_attributes="true"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_271890427" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_271890427" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#afd3f7" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#afd3f7"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_67550811">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_67550811"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="1. Opertations Fundamentals" POSITION="bottom_or_right" ID="ID_967967184" CREATED="1776448366938" MODIFIED="1776448959710">
+<edge COLOR="#ff0000"/>
+<node TEXT="Definitions" ID="ID_1836210580" CREATED="1776448386728" MODIFIED="1776448396395"/>
+<node TEXT="Process" ID="ID_1748980854" CREATED="1776448397244" MODIFIED="1776448409329"/>
+<node TEXT="Software Installation" ID="ID_161269253" CREATED="1776448415630" MODIFIED="1776448428245"/>
+<node TEXT="Scripting and Automation" ID="ID_27859085" CREATED="1776448437861" MODIFIED="1776448454215"/>
+<node TEXT="Effective Testing and Troubleshooting" ID="ID_298971873" CREATED="1776448454815" MODIFIED="1776448469017"/>
+<node TEXT="Performance Reliability and Load Balancing" ID="ID_1751456418" CREATED="1776448473292" MODIFIED="1776448489585"/>
+</node>
+<node TEXT="2. Operations Planning" POSITION="bottom_or_right" ID="ID_561484602" CREATED="1776448434113" MODIFIED="1776448966122">
+<edge COLOR="#00ff00"/>
+<node TEXT="Operations Plan and Supplier Management" ID="ID_154619169" CREATED="1776448513318" MODIFIED="1776448532145"/>
+<node TEXT="Development and Operations Environments." ID="ID_1984215066" CREATED="1776448532685" MODIFIED="1776448546066"/>
+<node TEXT="Software Availability Continuity and Service Levels" ID="ID_254137977" CREATED="1776448553376" MODIFIED="1776448580450"/>
+<node TEXT="Software Capacity Management" ID="ID_851208875" CREATED="1776448584730" MODIFIED="1776448597609"/>
+<node TEXT="Software Backup, Disaster Recover and Failover" ID="ID_864747849" CREATED="1776448600185" MODIFIED="1776448618025"/>
+<node TEXT="Software and Data Safety, Security, Integrity, Protection and Controls" ID="ID_1962376584" CREATED="1776448624313" MODIFIED="1776448658278"/>
+</node>
+<node TEXT="3. Operations Delivery" POSITION="top_or_left" ID="ID_1554040498" CREATED="1776448753338" MODIFIED="1776448971669">
+<edge COLOR="#00007c"/>
+<node TEXT="Operational Testing, Verification, and Acceptance" ID="ID_1067892283" CREATED="1776448804088" MODIFIED="1776448819634"/>
+<node TEXT="Deployment / Release Engineering" ID="ID_1751447236" CREATED="1776448823366" MODIFIED="1776448835946"/>
+<node TEXT="Rollback and Data Migration" ID="ID_1258288183" CREATED="1776448846389" MODIFIED="1776448858813"/>
+<node TEXT="Change Management" ID="ID_1456842841" CREATED="1776448868366" MODIFIED="1776448876800"/>
+<node TEXT="Problem Management" ID="ID_192294072" CREATED="1776448877838" MODIFIED="1776448885009"/>
+</node>
+<node TEXT="4. Operational Control" POSITION="top_or_left" ID="ID_594152294" CREATED="1776448890266" MODIFIED="1776448952912">
+<edge COLOR="#007c00"/>
+<node TEXT="Incident Management" ID="ID_1590034512" CREATED="1776448904936" MODIFIED="1776448914286"/>
+<node TEXT="Monitor, Measure, Track and Review" ID="ID_1895463788" CREATED="1776448920289" MODIFIED="1776448935978"/>
+<node TEXT="Operations Support" ID="ID_1398530043" CREATED="1776448976334" MODIFIED="1776448985708"/>
+<node TEXT="Operations Service Reporting" ID="ID_409827209" CREATED="1776448991392" MODIFIED="1776449014238"/>
+</node>
+<node TEXT="5. Practical Considerations" POSITION="top_or_left" ID="ID_1810764021" CREATED="1776449019813" MODIFIED="1776449052151">
+<edge COLOR="#7c007c"/>
+<node TEXT="Incident and Problem Prevention" ID="ID_53138168" CREATED="1776449053541" MODIFIED="1776449084827"/>
+<node TEXT="Operational and Risk Management" ID="ID_198121682" CREATED="1776449087822" MODIFIED="1776449103701"/>
+<node TEXT="Automated Software Engineering Operations" ID="ID_729347622" CREATED="1776449110727" MODIFIED="1776449128052"/>
+</node>
+<node TEXT="6. Operations Tools" POSITION="top_or_left" ID="ID_661511019" CREATED="1776449153253" MODIFIED="1776449172357">
+<edge COLOR="#007c7c"/>
+<node TEXT="Containers and Virtualization" ID="ID_1989226958" CREATED="1776449179048" MODIFIED="1776449193091"/>
+<node TEXT="Deployment" ID="ID_1404578485" CREATED="1776449194515" MODIFIED="1776449200383"/>
+<node TEXT="Automated Test" ID="ID_1177009310" CREATED="1776449204162" MODIFIED="1776449211851"/>
+<node TEXT="Monitoring and Telemetry" ID="ID_1956884156" CREATED="1776449213761" MODIFIED="1776449224612"/>
+</node>
+</node>
+</map>
